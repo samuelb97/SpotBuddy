95da181b4cb5bdeac920b83668d404850ad3a4f3
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2019-03-28 04:06:19 -0400

    message list

--- login/lib/src/search/search.dart
+++ login/lib/src/search/search.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:login/prop-config.dart';
 import 'package:login/analtyicsController.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 
 class SearchPage extends StatefulWidget {
@@ -12,6 +14,7 @@ class SearchPage extends StatefulWidget {
     @required this.user
   }) : super(key: key);
 
+
   final FirebaseUser user;
   final analyticsController analControl;
 
@@ -20,24 +23,104 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+
+  Widget buildItem(BuildContext context, DocumentSnapshot document) {
+    if (document['id'] == widget.user) {
+      return Container();
+    } else {
+      return Container(
+        child: FlatButton(
+          child: Row(
+            children: <Widget>[
+              Material(
+                child: CachedNetworkImage(
+                  placeholder: (context, url) => Container(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 1.0,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreen),
+                        ),
+                        width: 50.0,
+                        height: 50.0,
+                        padding: EdgeInsets.all(15.0),
+                      ),
+                  imageUrl: "assets/dog.jpg",  //TODO:profile pic image url from database
+                  width: 50.0,
+                  height: 50.0,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                clipBehavior: Clip.hardEdge,
+              ),
+              Flexible(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          'Name: ${document['name']}',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+                      ),
+                      Container(
+                        child: Text(
+                          'test',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                      )
+                    ],
+                  ),
+                  margin: EdgeInsets.only(left: 20.0),
+                ),
+              ),
+            ],
+          ),
+          // onPressed: () {
+          //   Navigator.push(
+          //       context,
+          //      MaterialPageRoute(
+          //           builder: (context) => Chat(
+          //                 peerId: document.documentID,
+          //                 peerAvatar: document['photoUrl'],
+          //             )));
+          // },
+          color: Colors.grey,
+          padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        ),
+        margin: EdgeInsets.only(bottom: 10.0, left: 5.0, right: 5.0),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     widget.analControl.currentScreen(Screens.search, Screens.searchOver);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          RaisedButton(
-              onPressed: () {},
-              child: Text(Headers.search),
+      body: 
+        Container(
+  child: StreamBuilder(
+    stream: Firestore.instance.collection('users').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return Center(
+          child: CircularProgressIndicator(
+			valueColor: AlwaysStoppedAnimation<Color>(Colors.lightGreen),
           ),
-          RaisedButton(
-              onPressed: () {},
-              child: Text(widget.user.email),
-          ),
-        ],
-      ),
-    );
-  }
+        );
+      } else {
+        return ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemBuilder: (context, index) => buildItem(context, snapshot.data.documents[index]),
+          itemCount: snapshot.data.documents.length,
+        );
+      }
+    },
+  ),
+        ));}
+ 
 }
